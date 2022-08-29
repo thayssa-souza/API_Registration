b0766be311fb6dd7e79be50c1be1bab78a82d947
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-08-29 18:26:15 -0300

    Add project files.

---
+++ Controller_Api/Cadastro.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Controller_Api
+{
+    public class Cadastro
+    {
+        public string Cpf { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public int Idade { get; set; }
+
+        public Cadastro(string nome, string cpf, DateTime dataNasc, int idade)
+        {
+            Cpf = cpf;
+            Nome = nome;
+            DataNascimento = dataNasc;
+            Idade = idade;
+        }
+
+        public Cadastro()
+        {
+
+        }
+    }
+}
---
+++ Controller_Api/Controllers/CadastroController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controller_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CadastroController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+        public List<Cadastro> cadastros = new()
+        {
+            new Cadastro("95896994052", "Buzz", Convert.ToDateTime(2000/05/10), 22),
+            new Cadastro("85503201027", "Woody", Convert.ToDateTime(1996/06/04), 26),
+            new Cadastro("29062467008", "Jessie", Convert.ToDateTime(1999/12/10), 22),
+            new Cadastro("34659205037", "Rex", Convert.ToDateTime(2001/03/27), 21),
+            new Cadastro("44288043000", "Andy", Convert.ToDateTime(2010/02/04), 12),
+        };
+
+        private readonly ILogger<CadastroController> _logger;
+
+        public CadastroController(ILogger<CadastroController> logger)
+        {
+            _logger = logger;
+            cadastros = cadastros.Select(cadastroPessoa => new Cadastro
+            {
+                Cpf = cadastroPessoa.Cpf,
+                Nome = cadastroPessoa.Nome,
+                DataNascimento = cadastroPessoa.DataNascimento,
+                Idade = cadastroPessoa.Idade,
+            })
+            .ToList();
+        }
+
+        //
+        [HttpGet]
+        public IEnumerable<Cadastro> Get()
+        {
+            return cadastros;
+        }
+
+        //
+        [HttpPost]
+        public IEnumerable<Cadastro> Post(Cadastro novoCadastro)
+        {
+            cadastros.Add(novoCadastro);
+            return cadastros;
+        }
+
+        [HttpPut]
+        public IEnumerable<Cadastro> Post(int index, Cadastro novoCadastro)
+        {
+            cadastros[index] = novoCadastro;
+            return cadastros;
+        }
+
+        [HttpDelete]
+        public IEnumerable<Cadastro> Delete(int index)
+        {
+            cadastros.RemoveAt(index);
+            return cadastros;
+        }
+    }
+}
